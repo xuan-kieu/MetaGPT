5863632509a195fc988939864040a649a12c1e5e
Ngô Xuân Kiều <tranthingocanh166@gmail.com>
2026-02-24 02:23:40 +0700

    17

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const specialistRoutes = require('./routes/specialistRoutes');
 const adminRoutes = require('./routes/adminRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 // Middleware
 app.use(cors({
