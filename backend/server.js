cda187bb56d8bb919446d74da81afcb789e8ec0d
Ngô Xuân Kiều <tranthingocanh166@gmail.com>
2026-02-24 01:22:38 +0700

    17

--- backend/server.js
+++ backend/server.js
@@ -13,13 +13,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
+aapp.use(cors({
     origin: [
       'http://localhost:5173',
-      'https://metagpt.vercel.app'
+      'https://meta-gpt-psi.vercel.app'
     ],
     credentials: true
-}));
+  }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
